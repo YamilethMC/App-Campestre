f221172fe0454c8aa0283dfe0f3a25eb0e986765
JesusVega2812 <l201708582@culiacan.tecnm.mx>
2025-10-01 01:18:59 -0700

    fix en importacion de modulo de menus

--- src/navigation/mainNavigator/index.tsx
+++ src/navigation/mainNavigator/index.tsx
@@ -16,7 +16,7 @@ import { COLORS } from '../../shared/theme/colors';
 // Screens
 import EventsScreen from '../../features/events/containers/EventsContainer';
 import HomeScreen from '../../features/home';
-import MenusScreen from '../../features/menus/containers/MenusContainer';
+import MenusScreen from '../../features/menus/containers';
 import ProfileScreen from '../../features/profile/containers';
 import SettingsScreen from '../../features/settings';
 import SurveysScreen from '../../features/surveys/containers/SurveysContainer';
