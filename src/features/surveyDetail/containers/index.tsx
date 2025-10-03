17702d366602b4fbc136b6397b0175ec94310cc9
YamilethMC <L21170016@culiacan.tecnm.mx>
2025-10-02 22:28:37 -0700

    Agregando componende de surveyDetail

---
+++ src/features/surveyDetail/containers/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const SurveysDetail: React.FC = () => {
+
+  return (
+    <View>
+      <Text>Survey Detail</Text>
+    </View>
+  );
+};
+
+
+export default SurveysDetail;
